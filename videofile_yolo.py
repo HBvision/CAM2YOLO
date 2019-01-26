aa83db34da941647faf189ec3ba1357930a5e7b3
HBVision <dragonquest2019@gmail.com>
2019-01-26 17:53:56 -0500

    hopefully this works

--- videofile_yolo.py
+++ videofile_yolo.py
@@ -1,9 +1,9 @@
 # -*- coding: utf-8 -*-
 
-import cv2
-from darkflow.net.build import TFNet
+import cv2 #imports openCV library
+from darkflow.net.build import TFNet #From the machine learning model, import the commands for detection
 import numpy as np
-import time
+import time #FPS/Time analysis
 
 options = {
         
@@ -13,36 +13,37 @@ options = {
         'gpu': 2.0
         }
 
-tfnet = TFNet(options)
+tfnet = TFNet(options) 
 
-capture = cv2.VideoCapture("video.mp4")
+capture = cv2.VideoCapture("video.mp4") #Loads in video from the same directory
 colors = [tuple(255 * np.random.rand(3)) for i in range(5)]
 
-frame_width = int( capture.get(cv2.CAP_PROP_FRAME_WIDTH))
+frame_width = int( capture.get(cv2.CAP_PROP_FRAME_WIDTH)) #Gather frame width and height
 frame_height =int( capture.get( cv2.CAP_PROP_FRAME_HEIGHT))
 
-fourcc = cv2.VideoWriter_fourcc(*'XVID')
+fourcc = cv2.VideoWriter_fourcc(*'XVID') #Formatting for displaying new video with detected objects
 
 out = cv2.VideoWriter('outpy.avi',fourcc, 15, (frame_width,frame_height))
+#writes video
 
 while (capture.isOpened()):
-    stime = time.time()
-    ret, frame = capture.read()
-    if ret:
-        results = tfnet.return_predict(frame)
+    stime = time.time() #setting a running time fro FPS
+    ret, frame = capture.read() # frame = current frame, and ret = next frame
+    if ret: #If there is a next frame
+        results = tfnet.return_predict(frame) #model detects objects, essentially a black box
         for color, result in zip(colors, results):
-            tl = (result['topleft']['x'], result['topleft']['y'])
-            br = (result['bottomright']['x'], result['bottomright']['y'])
-            label = result['label']
-            frame = cv2.rectangle(frame, tl, br, color, 7)
-            frame = cv2.putText(frame, label, tl, cv2.FONT_HERSHEY_COMPLEX, 1, (0, 0, 0), 2)
+            tl = (result['topleft']['x'], result['topleft']['y']) #dictionary with the top left x/y coordinates
+            br = (result['bottomright']['x'], result['bottomright']['y']) #same thing but with bottom right x/y coordinates
+            label = result['label'] #labels (need to adjust for detecting humans only)
+            frame = cv2.rectangle(frame, tl, br, color, 7) #OpenCV command for displaying all the bounding boxes
+            frame = cv2.putText(frame, label, tl, cv2.FONT_HERSHEY_COMPLEX, 1, (0, 0, 0), 2) #Puts labels on each bounding box with confidence levels
         out.write(frame)
         #cv2.imshow('frame',frame)    
         print('FPS {:.1f}'.format(1 / (time.time() - stime)))
-        if cv2.waitKey(1) & 0xFF == ord('q'):
+        if cv2.waitKey(1) & 0xFF == ord('q'): #just waits for q as an interrupt
             break
 
-capture.release()
-out.release()
-cv2.destroyAllWindows()
+capture.release() #essentially a close function for reading in the videos
+out.release() #close function for writing the videos
+cv2.destroyAllWindows() #clear every display when done
 print("finished")
